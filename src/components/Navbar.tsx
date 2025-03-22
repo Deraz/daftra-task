7c8c2ea076ad79f5b942922f9d80744916acdb28
Yehia Deraz <yehia.deraz@agileinnovationsgroup.com>
2025-03-23 01:57:25 +0200

    final updates and removing server from gitignore

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ const Navbar = () => {
     { title: "Profile ▼", icon: "/profile.png", hasMenu: true },
   ];
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="z-40 sticky top-0 flex justify-between items-center bg-black shadow-md px-4 py-2 flex-row-reverse md:flex-row">
       <div className="flex items-center gap-14">
@@ -87,7 +88,7 @@ const Navbar = () => {
                 item.hasMenu ? "relative" : ""
               }`}
               onClick={() => {
-                if (item.hasMenu) setIsOpen((prev) => !prev);
+                if (item.hasMenu) setIsOpen(true);
               }}
             >
               {item.isVerticalRule ? (
@@ -104,56 +105,73 @@ const Navbar = () => {
                   <span className="hover:opacity-80 text-sm">{item.title}</span>
                 </>
               )}
-              {item.hasMenu && isOpen && (
-                <div className="absolute top-[75px] left-0 mt-2 w-70 bg-white shadow-lg rounded-lg flex-col py-2 px-4">
-                  <div className="flex justify-between items-center">
-                    <div className="flex gap-2 items-center">
-                      <Image
-                        src={item.icon as string}
-                        alt={item.title as string}
-                        width={70}
-                        height={70}
-                      />
-                      <div className="flex flex-col">
-                        <span className="text-black">Ahmed Amaar</span>
-                        <span className="text-[#707070]">UX UI designer</span>
-                      </div>
-                    </div>
-                    <Image
-                      src="/chevron-right.png"
-                      alt="chevron"
-                      width={8}
-                      height={8}
-                      className="h-fit"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-2 text-[#707070] border-0 border-t border-solid border-[#F0F0F0] mt-4 -mx-4 p-4">
-                    {navItems
-                      .filter((i) => !i.hasMenu && !i.isVerticalRule)
-                      .map((item) => (
-                        <div
-                          className="flex gap-2 items-center"
-                          key={item.title}
-                        >
-                          <Image
-                            src={"/navbar-black" + item.icon}
-                            alt={item.title as string}
-                            width={20}
-                            height={20}
-                            className="hover:opacity-80"
-                          />
-                          <span className="hover:opacity-80">
-                            {item.title}
-                          </span>
+              {item.hasMenu && (
+                <div
+                  className={`bg-transparent  w-full h-screen fixed top-0 right-0 z-50 md:w-96 md:static md:h-auto md:z-30 
+                  transition-transform duration-500 ease-in-out flex
+                  ${
+                    isOpen ? "translate-x-0" : "translate-x-full"
+                  } md:translate-x-0`}
+                >
+                  <div
+                    className={` w-1/5 flex-1 z-50 duration-1000 transition-all ${
+                      isOpen ? "opacity-60 bg-black" : "bg-transparent opacity-0 pointer-events-none"
+                    }`}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsOpen(false);
+                    }}
+                  />
+                  <div className="w-5/6 max-w-[320px] p-4 bg-white">
+                    <div className="flex justify-between items-center">
+                      <div className="flex gap-2 items-center">
+                        <Image
+                          src={item.icon as string}
+                          alt={item.title as string}
+                          width={70}
+                          height={70}
+                        />
+                        <div className="flex flex-col">
+                          <span className="text-black">Ahmed Amaar</span>
+                          <span className="text-[#707070]">UX UI designer</span>
                         </div>
-                      ))}
+                      </div>
+                      <Image
+                        src="/chevron-right.png"
+                        alt="chevron"
+                        width={8}
+                        height={8}
+                        className="h-fit"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2 text-[#707070] border-0 border-t border-solid border-[#F0F0F0] mt-4 -mx-4 p-4">
+                      {navItems
+                        .filter((i) => !i.hasMenu && !i.isVerticalRule)
+                        .map((item) => (
+                          <div
+                            className="flex gap-2 items-center"
+                            key={item.title}
+                          >
+                            <Image
+                              src={"/navbar-black" + item.icon}
+                              alt={item.title as string}
+                              width={20}
+                              height={20}
+                              className="hover:opacity-80"
+                            />
+                            <span className="hover:opacity-80">
+                              {item.title}
+                            </span>
+                          </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-col gap-2 text-[#707070] border-0 border-y border-solid border-[#F0F0F0] mb-4 -mx-4 p-4">
+                      <span>Settings and privacy</span>
+                      <span>Language</span>
+                      <span>Help and privacy</span>
+                    </div>
+                    <div className="text-[#ED1F03] mb-2">Logout</div>
                   </div>
-                  <div className="flex flex-col gap-2 text-[#707070] border-0 border-y border-solid border-[#F0F0F0] mb-4 -mx-4 p-4">
-                    <span>Settings and privacy</span>
-                    <span>Language</span>
-                    <span>Help and privacy</span>
-                  </div>
-                  <div className="text-[#ED1F03] mb-2">Logout</div>
                 </div>
               )}
             </div>
